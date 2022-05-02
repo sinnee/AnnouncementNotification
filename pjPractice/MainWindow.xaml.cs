42231884a9d81c8f155275f0435cf17507ccf4a6
User <User@DESKTOP-P7EECRA>
2022-05-02 21:00:27 +0900

    초기 커밋.

---
+++ pjPractice/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace pjPractice
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected ChromeDriverService _driverService = null;
+        protected ChromeOptions _opt = null;
+        protected ChromeDriver _driver = null;
+
+        string number, category, title, date, seeing, webAddress;
+        Button[] buts = new Button[9];
+
+        string row;
+        string[] value = new string[7]; //number, category, title, date, seeing, webAddress, Isscrapped
+        string[] AllString;
+
+        int stringIndex = 1;
+
+        int pageNumber = 1;
+        int tempPage = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            leftButton.Click += Left_Click;
+            rightButton.Click += Right_Click;
+
+            buts[0] = notice1;
+            buts[1] = notice2;
+            buts[2] = notice3;
+            buts[3] = notice4;
+            buts[4] = notice5;
+            buts[5] = notice6;
+            buts[6] = notice7;
+            buts[7] = notice8;
+            buts[8] = notice9;
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _opt = new ChromeOptions();
+            _opt.AddArgument("disable-gpu");
+
+            //SearchWeb();
+
+            setStringvalue();
+            setBut(stringIndex - 1);
+        }
+
+        private void SearchWeb(object sender, RoutedEventArgs e)
+        {
+            StreamWriter sw = File.AppendText("test.txt");
+            int index = 3;
+
+            _opt.AddArgument("headless");
+
+            _driver = new ChromeDriver(_driverService, _opt);
+            _driver.Navigate().GoToUrl($"https://www.jbnu.ac.kr/kor/?menuID=139&pno={index}");
+            var element = _driver.FindElement(By.XPath("//*[@id='print_area']/div[2]/table/tbody/tr[1]/th"));
+
+            row = AllString[stringIndex - 1];
+            value = row.Split('\t');
+
+            int valueTemp, numberTemp;
+            int.TryParse(value[0], out valueTemp);
+
+            while (index > 0) { 
+                _driver.Navigate().GoToUrl($"https://www.jbnu.ac.kr/kor/?menuID=139&pno={index}");
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            
+                for (int i = 9; i > 0; i--)
+                {
+                    element = _driver.FindElement(By.XPath($"//*[@id='print_area']/div[2]/table/tbody/tr[{i}]/th"));
+                    number = element.Text;
+
+                    if(int.TryParse(number, out numberTemp))
+                    {
+                        if (valueTemp >= numberTemp)
+                            continue;
+                    }
+
+                    element = _driver.FindElement(By.XPath($"//*[@id='print_area']/div[2]/table/tbody/tr[{i}]/td[1]/span"));
+                    category = element.Text;
+
+                    element = _driver.FindElement(By.XPath($"//*[@id='print_area']/div[2]/table/tbody/tr[{i}]/td[2]/span/a"));
+                    title = element.Text;
+
+                    element = _driver.FindElement(By.XPath($"//*[@id='print_area']/div[2]/table/tbody/tr[{i}]/td[5]"));
+                    date = element.Text;
+
+                    element = _driver.FindElement(By.XPath($"//*[@id='print_area']/div[2]/table/tbody/tr[{i}]/td[6]"));
+                    seeing = element.Text;
+
+                    element = _driver.FindElement(By.XPath($"//*[@id='print_area']/div[2]/table/tbody/tr[{i}]/td[2]/span/a"));
+                    webAddress = element.GetAttribute("href");
+
+                    sw.WriteLine($"{number}\t{category}\t{title}\t{date}\t{seeing}\t{webAddress}\t0");
+                }
+                index--;
+            }
+            sw.Close();
+        }
+
+        private void setBut(int n)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                if (n < 0 || n > stringIndex - 1) {
+                    return;
+                }
+                    
+                row = AllString[n];
+                value = row.Split('\t');
+                buts[i].Content = $"{value[0]} {value[1]} {value[2]} {value[3]}";
+                n--;
+            }
+            tempPage = n;
+        }
+
+        private void setStringvalue()
+        {
+            StreamReader sr = new StreamReader(new FileStream("test.txt", FileMode.Open));
+            string temp = sr.ReadToEnd();
+            stringIndex = temp.Split('\n').Length - 1;
+
+            AllString = new string[stringIndex];
+            AllString = temp.Split('\n');
+
+            pageNumber = stringIndex;
+            sr.Close();
+        }
+
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {
+            if (pageNumber >= AllString.Length - 1)
+                return;
+
+            pageNumber += 9;
+            setBut(pageNumber);
+        }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+            if (pageNumber <= 0)
+                return;
+            pageNumber = tempPage;
+            setBut(pageNumber);
+        }
+    }
+}
